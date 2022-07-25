f5ea39709eac49259ade9ef2b261eca62fcb39f4
Albiddd <alberto.dore93@gmail.com>
2022-07-25 16:39:37 +0200

    ex bonus

---
+++ bonus/app.js
@@ -0,0 +1,23 @@
+const numElementi = parseInt( prompt('Inserisci il numero di elementi dell`array'))
+const numEstrazioni = parseInt( prompt('Inserisci il numero di elementi da estrarre partendo dalla fine'))
+const myArray = []
+
+if (numEstrazioni >= numElementi){
+    alert('ATTENZIONE! Non puoi estrarre un numero di elementi maggiore del numero totale')
+}
+else{
+    // RIEMPIO L'ARRAY DI NUMENRI
+    for(i=0; i<numElementi; i++){
+    
+    const arrayElement = Math.round(Math.random()*100);
+
+    myArray.push(arrayElement)
+    }
+}
+// STAMPO GLI ELEMENTI DELL'ARRAY
+console.log(myArray)
+
+// STAMPO GLI ELEMENTI PARTENDO DALLA FINE DELL'ARRAY
+for(i=(numElementi-1); i>=numEstrazioni; i-- ){
+    console.log(myArray[i])
+}
--- dadi/dadi.js
+++ dadi/dadi.js
@@ -1,11 +1,13 @@
  document.querySelector('input[type="submit"').addEventListener("click",function(){
-	var num1=Math.round(Math.random()*6);
-	var num2=Math.round(Math.random()*6);
-			
-	document.getElementById("numero1").innerHTML ="Hai ottenuto: " + num1;
-	document.getElementById("numero2").innerHTML ="L`avversario ha ottenuto: " + num2;
+    // GENERO I DUE DADI
+	var num1 = Math.round(Math.random()*6);
+	var num2 = Math.round(Math.random()*6);
 	
-			
+    // STAMPO IL RISULTATO DEI DADI
+	document.getElementById("numero1").innerHTML = "Hai ottenuto: " + num1;
+	document.getElementById("numero2").innerHTML = "L`avversario ha ottenuto: " + num2;
+	
+	// STAMPO L'ESITO DELLA PARTITA CONFRONTANDO I RISULTATI 
 	if(num1 > num2){
         document.getElementById("esito").innerHTML = "Hai vinto!";
     }
